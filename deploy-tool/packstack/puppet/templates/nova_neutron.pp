2612668e86675642eded849cdb9532662f5a7d5d
cylee <cyber.cylee@gmail.com>
2014-07-01 17:23:42 +0800

    fix vif binding error

--- deploy-tool/packstack/puppet/templates/nova_neutron.pp
+++ deploy-tool/packstack/puppet/templates/nova_neutron.pp
@@ -5,6 +5,9 @@ class {"nova::network::neutron":
   neutron_url => "http://%(CONFIG_NEUTRON_SERVER_HOST)s:9696",
   neutron_admin_tenant_name => "services",
   neutron_admin_auth_url => "http://%(CONFIG_KEYSTONE_HOST)s:35357/v2.0",
+  # cylee : Check out http://kashyapc.fedorapeople.org/virt/openstack/rdo/IceHouse-Nova-Neutron-ML2-GRE-OVS.txt
+  vif_plugging_is_fatal => false,
+  vif_plugging_timeout  => 0,
 }
 
 class {"nova::compute::neutron":
